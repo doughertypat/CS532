4a957193d8b7b56dfb7e7e1e718d75ed05b68a69
Dougherty <patrick.r.dougherty@talonhead2.cm.cluster>
2020-09-13 15:22:38 -0500

    Finish hw1 program

--- HW1/hw1_DoughertyP.cpp
+++ HW1/hw1_DoughertyP.cpp
@@ -1,5 +1,5 @@
 /************************************************
- * CS532 - Lab 3
+ * CS532 - HW1
  * Patrick Dougherty
  * patrick.r.dougherty@und.edu
  * 12 Sep 20
@@ -12,66 +12,72 @@
 
 
 /*
- * Write a function that uses MPI to broadcast a message from rank 0 
- * to all other ranks in the current MPI_COMM_WORLD. Use only MPI_Send
- * and MPI_Recv in your implementation.
+ * Write an MPI program with two tasks that alter and send a numeric message back and forth for
+ * a set number of iterations. The root task shall add two to a number, print it to the console, send
+ * the number to the other task, and receive the new value from the non-root task. The other task
+ * shall receive the number, add 20 to it, and send the updated value back to the root task. The
+ * number of iterations shall be input to the program with a command line argument. 
  */ 
 
-void mybcast(void *array, int array_len, int my_rank, int comm_size) {
+
+void incrementNumber(int *data, int my_rank)
+{
     if (my_rank == 0)
     {
-        for (int i = 1; i < comm_size; i++)
-        {
-            MPI_Send(array, array_len, MPI_INT, i, 0, MPI_COMM_WORLD);
-        }
+       *data += 2;
     }
     else
     {
-        MPI_Recv(array, array_len, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+       *data += 20;
     }
-}
-
-void printArray(int *array, int array_len, int my_rank) {
-    using std::cout;
-    using std::endl;
-    using std::ostringstream;
-
-    ostringstream oss;
-    oss << "[rank: " << my_rank << "]: ";
-    for (int i = 0; i < array_len; i++) {
-        oss << " " << array[i];
-    }
-    cout << oss.str() << endl;
-
-    MPI_Barrier(MPI_COMM_WORLD);
 }
 
 int main(int argc, char *argv[]) {
-    int array_len = 20;
-    int *array = new int[array_len];
-
+    int value = 0;
+    int iterations;
     int comm_size;
     int my_rank;
+    
+    if (argc != 2)
+    {
+        std::cout << "Improper usage: hw1_DoughertyP <# of iterations>" << std::endl;
+        return 0;
+    }
+    iterations = atoi(argv[1]); 
 
     MPI_Init(NULL, NULL);
     MPI_Comm_size(MPI_COMM_WORLD, &comm_size);
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
-    if (my_rank == 0) {
-        for (int i = 0; i < array_len; i++) {
-            array[i] = i;
+    if(my_rank == 0)
+    {
+        std::cout << "Starting value: " << value << std::endl;
+    }
+
+    for (int i = 0; i < iterations; i++)
+    {
+        if(my_rank == 0)
+        {
+           incrementNumber(&value, my_rank);
+           std::cout << "New value: " << value << std::endl;
+           MPI_Send(&value, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+           MPI_Recv(&value, 1, MPI_INT, 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         }
-    } else {
-        for (int i = 0; i < array_len; i++) {
-            array[i] = 0;
+        else
+        {
+            MPI_Recv(&value, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            incrementNumber(&value, my_rank);
+            MPI_Send(&value, 1, MPI_INT, 0, 0, MPI_COMM_WORLD); 
         }
     }
 
-    printArray(array, array_len, my_rank);
-    mybcast(array, array_len, my_rank, comm_size);
-    printArray(array, array_len, my_rank);
+    if(my_rank == 0)
+    {
+        std::cout << "Final value: " << value << std::endl;
+    }
 
     MPI_Finalize();
+    
     return 0;
 }
 
